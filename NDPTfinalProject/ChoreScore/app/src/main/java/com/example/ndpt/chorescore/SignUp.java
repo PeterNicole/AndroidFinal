03ece5f280ccdd57bf0acc33bd9b3caf968893d5
Nicole Dahlquist <ndahlquist-cc@conestogac.on.ca>
2015-11-20 12:00:28 -0500

    onClickListeners for main_options fragment
    booyah

--- NDPTfinalProject/ChoreScore/app/src/main/java/com/example/ndpt/chorescore/SignUp.java
+++ NDPTfinalProject/ChoreScore/app/src/main/java/com/example/ndpt/chorescore/SignUp.java
@@ -1,11 +1,12 @@
 package com.example.ndpt.chorescore;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
