94a6e1e5895bb6b764d312943c7ea831fa406cbe
pjthomso <peter.thomson@live.ca>
2015-11-20 11:54:34 -0500

    Added TransitionManager Class

---
+++ NDPTfinalProject/ChoreScore/app/src/main/java/com/example/ndpt/chorescore/TransitionManager.java
@@ -0,0 +1,64 @@
+package com.example.ndpt.chorescore;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * TransitionManager.java
+ * Created by Peter Thomson on 20/11/2015.
+ *
+ * Class for simplification of opening/closing activities and fragments
+ */
+
+public class TransitionManager
+{
+
+    private static Class previousActivity = MainActivity.class;
+
+    /**
+     * Overloaded method for default closeCurrent = true
+     * @param currentActivity current activity
+     * @param transitionActivity activity to be opened
+     */
+    static public void ActivityTransition(AppCompatActivity currentActivity,  Class transitionActivity)
+    {
+        ActivityTransition(currentActivity, transitionActivity, true);
+    }
+
+    /**
+     * Transitions to the specified activity
+     * @param currentActivity current activity
+     * @param transitionActivity activity to be opened
+     * @param closeCurrent if true close the current activity
+     */
+    static public void ActivityTransition(AppCompatActivity currentActivity, Class transitionActivity, boolean closeCurrent)
+    {
+        previousActivity = currentActivity.getClass();
+        Intent intent = new Intent(currentActivity,transitionActivity);
+        currentActivity.startActivity(intent);
+        if(closeCurrent){currentActivity.finish();}
+    }
+
+    /**
+     * Transitions to the last known activity which was opened
+     * @param currentActivity current activity
+     * @param closeCurrent if true close the current activity
+     */
+    static public void PreviousActivity(AppCompatActivity currentActivity, boolean closeCurrent)
+    {
+        Intent intent = new Intent(currentActivity,previousActivity.getClass());
+        currentActivity.startActivity(intent);
+        if(closeCurrent){currentActivity.finish();}
+    }
+
+    /**
+     * For opening fragments on current activity, work in progress
+     */
+    static public void FragmentTransition()
+    {
+        //Not sure yet
+    }
+}
