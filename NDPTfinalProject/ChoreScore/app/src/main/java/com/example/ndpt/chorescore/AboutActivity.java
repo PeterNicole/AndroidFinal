b7bf93ff1897fb2b2327116fdb860679bbf46c52
pjthomso <peter.thomson@live.ca>
2015-12-10 22:16:02 -0500

    removed menus from about, login page

--- NDPTfinalProject/ChoreScore/app/src/main/java/com/example/ndpt/chorescore/AboutActivity.java
+++ NDPTfinalProject/ChoreScore/app/src/main/java/com/example/ndpt/chorescore/AboutActivity.java
@@ -23,13 +23,6 @@ public class AboutActivity extends Activity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_groups, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
--- NDPTfinalProject/ChoreScore/app/src/main/java/com/example/ndpt/chorescore/MainActivity.java
+++ NDPTfinalProject/ChoreScore/app/src/main/java/com/example/ndpt/chorescore/MainActivity.java
@@ -34,26 +34,6 @@ public class MainActivity extends Activity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // No menu
-        getMenuInflater().inflate(R.menu.menu_groups, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        TransitionManager.MenuTransition(this, id);
-
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onFragmentInteraction(Uri uri)
     {
 
