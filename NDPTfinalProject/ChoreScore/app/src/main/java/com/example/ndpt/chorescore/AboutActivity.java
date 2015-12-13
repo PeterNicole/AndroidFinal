9f33c4df1e9ecc528b01571947b513689f40fa48
pjthomso <peter.thomson@live.ca>
2015-12-13 16:02:24 -0500

    added user manual button and web page load

--- NDPTfinalProject/ChoreScore/app/src/main/java/com/example/ndpt/chorescore/AboutActivity.java
+++ NDPTfinalProject/ChoreScore/app/src/main/java/com/example/ndpt/chorescore/AboutActivity.java
@@ -1,10 +1,14 @@
 package com.example.ndpt.chorescore;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
 /**
  * AboutActivity.java
  * Created by Nicole Dahlquist on 21/11/2015.
@@ -16,11 +20,15 @@ public class AboutActivity extends Activity
         AboutTwoFragment.OnFragmentInteractionListener,
         GoBackButtonFragment.OnFragmentInteractionListener{
 
+    //Class scope variables
+    private Button manualButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_about);
+            CreateControls();
         }
         catch(Exception e) {
             System.out.println("Error " + e.getMessage());
@@ -48,6 +56,48 @@ public class AboutActivity extends Activity
 
     @Override
     public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    /**
+     * Initializes controls on the about activity
+     */
+    public void CreateControls()
+    {
+        try
+        {
+            //Set on click event to download the user manual
+            manualButton = (Button)findViewById(R.id.about_manual_btn);
+            manualButton.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    ShowManual();
+                }
+            });
+        }
+
+        catch(Exception e)
+        {
+            System.out.println("Error " + e.getMessage());
+        }
+    }
+
+    /**
+     * Opens browser for the user manual
+     */
+    public void ShowManual()
+    {
+        try
+        {
+            Intent intent= new Intent(Intent.ACTION_VIEW,Uri.parse("https://drive.google.com/file/d/0B7vtDsy6XVruVFY5NFd6V09QMzg/view?usp=sharing"));
+            startActivity(intent);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error " + e.getMessage());
+        }
 
     }
 }
